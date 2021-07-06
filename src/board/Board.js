66c8e6a9b9ecf1036f25403b3cad31f7c0ec87ed
Tong Ye <tong.ye@stereye.com>
2021-07-06 19:56:02 +0800

    UPDATE

---
+++ src/board/Board.js
@@ -0,0 +1,93 @@
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import { OrbitControls } from 'threeJSM/controls/OrbitControls.js'
+
+class Board {
+    renderer
+    camera
+    scene
+    controls
+    #animatdId
+
+    /**
+     *
+     * @param {HTMLElement} el
+     */
+    constructor(el) {
+        this.renderer = this.initRenderer(el)
+        this.camera = this.initCamera()
+        this.scene = this.initScene()
+        this.controls = this.initControls()
+    }
+
+    /**
+     *
+     * @param {HTMLElement} el
+     * @returns {WebGLRenderer} renderer
+     */
+    initRenderer(el) {
+        const cav = document.createElement('canvas')
+        cav.style.height = '100vh'
+        cav.style.width = '100vw'
+        el.appendChild(cav)
+
+        const renderer = new WebGLRenderer({
+            canvas: cav,
+            alpha: true,
+            premultipliedAlpha: false,
+            antialias: true,
+            sortObjects: false,
+            autoClear: false,
+        })
+
+        renderer.setSize(window.innerWidth, window.innerHeight)
+
+        return renderer
+    }
+
+    /**
+     *
+     * @returns {PerspectiveCamera}
+     */
+    initCamera() {
+        const camera = new PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            100
+        )
+        return camera
+    }
+
+    /**
+     *
+     * @returns {Scene}
+     */
+    initScene() {
+        const scene = new Scene()
+        return scene
+    }
+
+    /**
+     *
+     * @returns {OrbitControls}
+     */
+    initControls() {
+        const controls = new OrbitControls(
+            this.camera,
+            this.renderer.domElement
+        )
+
+        return controls
+    }
+
+    setOpt(opt) {}
+
+    update() {
+        this.renderer.render(this.scene, this.camera)
+        if (!this.#animatdId) {
+            this.#animatdId = requestAnimationFrame(() => this.update())
+        }
+    }
+}
+
+export { Board }
